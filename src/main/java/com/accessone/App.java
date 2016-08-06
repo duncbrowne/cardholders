da9ced464b95ee021b59348ea5fe216747b1fb41
Duncan Browne <duncbrowne@gmail.com>
2016-08-06 17:10:03 +0100

    Made it so the message gets sent to the RabbitMQ queue.  The jar file is made as a full assembly.

--- src/main/java/com/accessone/App.java
+++ src/main/java/com/accessone/App.java
@@ -4,10 +4,29 @@ package com.accessone;
  * Hello world!
  *
  */
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Channel;
+
 public class App 
 {
-    public static void main( String[] args )
+    private final static String QUEUE_NAME = "TestQueue";
+
+    public static void main( String[] args ) throws java.io.IOException, java.util.concurrent.TimeoutException
     {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+        String message = "Hello World!";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
+
         System.out.println( "Hello World!" );
     }
 }
